432293c4be63deaca3ae9b777cbb5e19d2b9b0b6
4Cafes <138708258+4Cafes@users.noreply.github.com>
2023-11-07 05:13:13 -0800

    Update fpsunlocker.py

--- fpsunlocker.py
+++ fpsunlocker.py
@@ -1,13 +1,28 @@
-#thanks for using my project
-
-import os
-import json
 import time
+print("Imported TIME")
+time.sleep(0.2)
+import json
+print("Imported JSON")
+time.sleep(0.2)
+import os
+print("Imported OS")
+time.sleep(0.2)
 import glob
+print("Imported GLOB")
+time.sleep(0.2)
 import ctypes
+print("Imported CTYPES")
+time.sleep(0.2)
+import platform
+print("Imported PLATFORM")
+time.sleep(0.2)
+
 
 def set_console_title(title):
-    ctypes.windll.kernel32.SetConsoleTitleW(title)
+    if platform.system() == "Windows":
+        ctypes.windll.kernel32.SetConsoleTitleW(title)
+    elif platform.system() == "Darwin":  
+        print(f"\033]0;{title}\a", end='', flush=True)
 
 new_title = "Cafes.lol FPS Unlocker"
 set_console_title(new_title)
@@ -54,7 +69,7 @@ class RobloxFFlags:
 
     def set_target_fps(self, cap_opt):
         if cap_opt is not None:
-            cap_opt = 5588562 if cap_opt == 0 else cap_opt
+            cap_opt = 9999999999 if cap_opt == 0 else cap_opt
         if self.update_flag("DFIntTaskSchedulerTargetFps", cap_opt):
             self.target_fps_mod = True
         return self
@@ -77,61 +92,115 @@ class RobloxFFlags:
         return False
 
 def find_newest_version():
-    versions_dir = "C:\\Users\\cafes\\AppData\\Local\\Roblox\\Versions\\"
+    username = os.getlogin()
+    versions_dir = f"C:\\Users\\{username}\\AppData\\Local\\Roblox\\Versions\\"
     version_folders = glob.glob(os.path.join(versions_dir, "*"))
     newest_version_folder = max(version_folders, key=os.path.getctime)
     return newest_version_folder
 
 def main():
-    while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print("\033[94mCafes.lol FPS Unlocker\033[0m")
-        print("1. FPS Unlocker")
-        print("2. Check Current FPS Lock")
-        print("3. Credits")
-        print("4. Exit")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Select your operating system:")
+    print("1. Windows")
+    print("2. Mac [UNTESTED PLEASE TELL US IF IT WORKS IN OUR DISCORD]")
+    choice = input("Enter your OS: ")
 
-        choice = input("Enter your choice: ")
+    if choice == "1":
+        newest_version = find_newest_version()
+        if newest_version:
+            json_file_path = os.path.join(newest_version, "ClientSettings", "ClientAppSettings.json")
 
-        if choice == "1":
-            newest_version = find_newest_version()
-            flags = RobloxFFlags(newest_version)
-            try:
-                target_fps_value = int(input("Enter the desired FPS value (0 to remove limit): "))
-                flags.set_target_fps(target_fps_value)
-                flags.set_alt_enter_flag(True)
-                print(f"Roblox version found: {os.path.basename(newest_version)}")
+        if json_file_path is None or not os.path.exists(json_file_path):
+            print("ROBLOX file not found. This code will not work.")
+            time.sleep(2)
+            exit(1)
+        
+        while True:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("\033[94mCafes.lol FPS Unlocker\033[0m")
+            print("1. FPS Unlocker")
+            print("2. Check Current FPS Lock")
+            print("3. Credits")
+            print("4. Exit")
+        
+            choice = input("Enter your choice: ")
+        
+            if choice == "1":
+                flags = RobloxFFlags(newest_version)
+                try:
+                    target_fps_value = int(input("Enter the desired FPS value (0 to remove limit): "))
+                    flags.set_target_fps(target_fps_value)
+                    flags.set_alt_enter_flag(True)
+                    print(f"Roblox version found: {os.path.basename(newest_version)}")
 
-                if flags.apply(True):
-                    print("\033[92mFPS Unlocker Successful\033[0m")
+        
+                    if flags.apply(True):
+                        print("\033[92mFPS Unlocker Successful\033[0m")
+                        time.sleep(5)
+                    else:
+                        print("\033[91mFPS Not Changed\033[0m")
+                        time.sleep(2.5)
+                except ValueError:
+                    print("\033[91mInvalid Input.\033[0m")
+                    time.sleep(2.5)
+            elif choice == "2":
+                flags = RobloxFFlags(newest_version)
+                target_fps = flags.target_fps()
+                if target_fps is not None:
+                    print(f"Current Target FPS: {target_fps}")
                     time.sleep(5)
                 else:
-                    print("\033[91mFPS Not Changed\033[0m")
+                    print("No Target FPS value found. Redo the FPS Unlocker process if this continues. Go to our Discord on cafes.lol for help.")
                     time.sleep(2.5)
-            except ValueError:
-                print("\033[91mInvalid Input.\033[0m")
-                time.sleep(2.5)
-        elif choice == "2":
-            newest_version = find_newest_version()
-            flags = RobloxFFlags(newest_version)
-            target_fps = flags.target_fps()
-            if target_fps is not None:
-                print(f"Current Target FPS: {target_fps}")
-                time.sleep(5)
+            elif choice == "3":
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print("Made by 4Cafes")
+                print("Discord.gg/jQReR3tKBj")
+                print("cafes.lol/fpsunlocker")
+                input("Press Enter to return to the main menu...")
+            elif choice == "4":
+                print("Exiting...")
+                time.sleep(2)
+                break
             else:
-                print("No Target FPS value set.")
-                time.sleep(2.5)
-        elif choice == "3":
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print("Made by 4Cafes")
-            print("Discord.gg/jQReR3tKBj")
-            print("cafes.lol/fpsunlocker")
-            input("Press Enter to return to the main menu...")
-        elif choice == "4":
-            print("Exiting...")
-            time.sleep(2)
-            break
-        else:
-            print("Invalid choice. Please select a valid option.")
+                print("Invalid choice. Please select a valid option.")
+    elif choice == "2":
+        os.system('cls' if os.name == 'nt' else 'clear')
+        while True:
+            try:
+                target_fps_input = input("Enter the FPS value you want the new cap to be: ")
+                targetFpsMac = int(target_fps_input)
+                break
+            except ValueError:
+                print("Invalid input. Please enter a valid integer value for FPS.")
+
+        robloxPath = "/Applications/Roblox.app"
+
+        if not os.path.exists(robloxPath):
+            robloxPath = os.path.expanduser("~") + robloxPath
+
+        if not os.path.exists(robloxPath):
+            print("Roblox installation folder couldn't be found. Are you on Windows?")
+            time.sleep(5)
+            exit()
+
+        clientSettingsPath = os.path.join(robloxPath, "Contents/MacOS/ClientSettings")
+
+        if not os.path.exists(clientSettingsPath):
+            os.mkdir(clientSettingsPath)
+
+        settings = {"DFIntTaskSchedulerTargetFps": targetFpsMac}
+        settings_file = os.path.join(clientSettingsPath, "ClientAppSettings.json")
+
+        with open(settings_file, "w") as f:
+            json.dump(settings, f, indent=2)
+
+        print("\033[92mFPS Unlocker Successful\033[0m")
+        time.sleep(5)
+        os._exit(1)
+    
+    else:
+        print("Invalid choice. Please select a valid option.")
+
 if __name__ == "__main__":
     main()
